30f32a311cd59b1a19e6c61f543515b94088b57d
PraveenP9105 <2k22cse117@kiot.ac.in>
2026-02-03 20:42:09 +0530

    Version 1 Completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const API_URL = "https://restcountries.com/v3.1/all?fields=name,flags,capital";
 const container = document.getElementById("countries-container");
+const pagination = document.getElementById("pagination");
+
+let countriesData = [];
+let currentPage = 1;
+const itemsPerPage = 16;
 
 async function fetchCountries() {
     try{
@@ -7,20 +12,22 @@ async function fetchCountries() {
         if(!response.ok) {
             throw new Error(`Failed to fetch countries: ${response.status} ${response.statusText}`);
         }
-        const countries = await response.json();
-        console.log("Countries fetch successful", countries);
-        console.log("Total countries: ", countries.length);
-
-        displayCountries(countries);
+        countriesData = await response.json();
+        renderPage();
+        // console.log("Countries fetch successful", countries);
+        // console.log("Total countries: ", countries.length);
     }
     catch (error) {
         console.log("Error fetching countries:", error.message);
     }
 }
 
-function displayCountries(countries) {
+function renderPage(){
     container.innerHTML = "";
-    countries.forEach(country => {
+    const stIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = stIndex + itemsPerPage;
+    const curCountries = countriesData.slice(stIndex, endIndex);
+    curCountries.forEach(country => {
         const card = document.createElement("div");
         card.className = "country-card";
         card.innerHTML = 
@@ -29,6 +36,23 @@ function displayCountries(countries) {
         <div class="country-capital">Capital: ${country.capital ? country.capital[0] : "No capital"}</div>`;
         container.appendChild(card);
     });
+    renderPagination();
+}
+
+function renderPagination(){
+    pagination.innerHTML = "";
+    const totalPages = Math.ceil(countriesData.length / itemsPerPage);
+    for(let i = 1; i <= totalPages; i++){
+        const btn = document.createElement("button");
+        btn.textContent = i;
+        if(i === currentPage)
+            btn.classList.add("active");
+        btn.addEventListener("click", () => {
+            currentPage = i;
+            renderPage();
+        });
+        pagination.appendChild(btn);
+    }
 }
 
 fetchCountries();
